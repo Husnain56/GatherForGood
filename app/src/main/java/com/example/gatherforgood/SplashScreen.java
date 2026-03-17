8c66c6ba50a4d4fe7fddc02be1c2e228acf4add5
Husnain <husnainhere.2019.5@gmail.com>
2026-03-17 17:32:57 +0500

    feat: add blinking dot animation to splash screen

--- app/src/main/java/com/example/gatherforgood/SplashScreen.java
+++ app/src/main/java/com/example/gatherforgood/SplashScreen.java
@@ -3,6 +3,9 @@ package com.example.gatherforgood;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SplashScreen extends AppCompatActivity {
+
+    Animation blink;
+    View statusDot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +28,18 @@ public class SplashScreen extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        init();
+        ApplyAnimation();
 
+    }
 
+    public void init(){
+        statusDot = findViewById(R.id.statusDot);
+    }
+
+    public void ApplyAnimation(){
+        blink = AnimationUtils.loadAnimation(this,R.anim.anim_blink);
+        statusDot.setAnimation(blink);
     }
 
 }
