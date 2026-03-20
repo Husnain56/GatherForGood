98978ce14675b0c2a7203a68dd0753925d325db2
Husnain <husnainhere.2019.5@gmail.com>
2026-03-20 21:56:02 +0500

    feat: set up ViewPager2 and adapter for onboarding screens

---
+++ app/src/main/java/com/example/gatherforgood/OnboardingScreen.java
@@ -0,0 +1,31 @@
+package com.example.gatherforgood;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+public class OnboardingScreen extends AppCompatActivity {
+
+    ViewPager2 vpOnboarding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_onboarding_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void init(){
+        vpOnboarding = findViewById(R.id.vpOnboarding);
+    }
+}
