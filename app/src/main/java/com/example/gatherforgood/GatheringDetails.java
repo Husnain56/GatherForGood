210676fc6a47f6b9167cfdd457e08f7e18a61713
Husnain <husnainhere.2019.5@gmail.com>
2026-03-24 19:45:12 +0500

    feat: connect GatheringDetails UI to PrayerGathering data model

--- app/src/main/java/com/example/gatherforgood/GatheringDetails.java
+++ app/src/main/java/com/example/gatherforgood/GatheringDetails.java
@@ -1,6 +1,10 @@
 package com.example.gatherforgood;
 
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +12,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+
 public class GatheringDetails extends AppCompatActivity {
 
+
+    TextView tvStatus;
+    TextView tvTitle;
+    TextView tvDescription;
+    TextView tvPrayerTime;
+    TextView tvPrayerName;
+    TextView tvHostName;
+    TextView tvAttendingCount;
+
+    MaterialButton btnOpenMaps;
+    ImageButton btnBack;
+    PrayerGathering gathering;
+    TextView tvLocationDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +39,39 @@ public class GatheringDetails extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        init();
+        bindData();
+        setEventListeners();
+    }
+
+    public void init(){
+        tvStatus = findViewById(R.id.tvStatus);
+        tvTitle = findViewById(R.id.tvTitle);
+        tvDescription = findViewById(R.id.tvDescription);
+        tvPrayerTime = findViewById(R.id.tvPrayerTime);
+        tvPrayerName = findViewById(R.id.tvPrayerName);
+        tvHostName = findViewById(R.id.tvHostName);
+        tvAttendingCount = findViewById(R.id.tvAttendingCount);
+        btnOpenMaps = findViewById(R.id.btnOpenMaps);
+        btnBack = findViewById(R.id.btnBack);
+        tvLocationDescription = findViewById(R.id.tvLocationDescription);
+        gathering = (PrayerGathering) getIntent().getSerializableExtra("prayer_gathering");
+    }
+
+    public void bindData(){
+        tvStatus.setText(gathering.getStatus());
+        tvTitle.setText(gathering.getPrayerType());
+        tvDescription.setText(gathering.getDescription());
+        tvPrayerTime.setText(gathering.getTime());
+        tvPrayerName.setText(gathering.getPrayerType());
+        tvHostName.setText(gathering.getHostName());
+        tvAttendingCount.setText(gathering.getParticipantCount() + " Attending");
+        tvLocationDescription.setText(gathering.getLocation());
+    }
+    public void setEventListeners(){
+        btnBack.setOnClickListener(v -> {
+            finish();
         });
     }
 }
