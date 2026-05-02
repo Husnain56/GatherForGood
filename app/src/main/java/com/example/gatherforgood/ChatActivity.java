9d83830f87a8925314f22bc1bb2179ea1e8272f1
Husnain <husnainhere.2019.5@gmail.com>
2026-05-02 18:04:19 +0500

    ui: add activity_chat layout and fix keyboard overlap with adjustResize

--- app/src/main/java/com/example/gatherforgood/ChatActivity.java
+++ app/src/main/java/com/example/gatherforgood/ChatActivity.java
@@ -1,6 +1,7 @@
 package com.example.gatherforgood;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,8 +14,8 @@ public class ChatActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_chat);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
